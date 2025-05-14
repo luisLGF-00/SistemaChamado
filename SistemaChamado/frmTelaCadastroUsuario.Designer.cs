348b34ceb9c77a0a23ab1549100aa25c83841d89
Senai <Senai@CEPEAS21104035>
2025-05-13 21:47:21 -0300

    cadastro de novo usuario inserido no banco

--- SistemaChamado/frmTelaCadastroUsuario.Designer.cs
+++ SistemaChamado/frmTelaCadastroUsuario.Designer.cs
@@ -32,12 +32,14 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.lbSenha = new System.Windows.Forms.Label();
-            this.txtSenhaCadastro = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtNomeCadastro = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lbDiretorio = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -70,7 +72,7 @@
             this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCadastrar.ForeColor = System.Drawing.Color.White;
-            this.btnCadastrar.Location = new System.Drawing.Point(267, 329);
+            this.btnCadastrar.Location = new System.Drawing.Point(258, 385);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(112, 39);
             this.btnCadastrar.TabIndex = 26;
@@ -83,22 +85,22 @@
             this.lbSenha.AutoSize = true;
             this.lbSenha.BackColor = System.Drawing.Color.White;
             this.lbSenha.Font = new System.Drawing.Font("Adobe Heiti Std R", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbSenha.Location = new System.Drawing.Point(39, 244);
+            this.lbSenha.Location = new System.Drawing.Point(33, 294);
             this.lbSenha.Name = "lbSenha";
             this.lbSenha.Size = new System.Drawing.Size(60, 20);
             this.lbSenha.TabIndex = 24;
             this.lbSenha.Text = "Senha:";
             // 
-            // txtSenhaCadastro
+            // txtSenha
             // 
-            this.txtSenhaCadastro.BackColor = System.Drawing.Color.Gainsboro;
-            this.txtSenhaCadastro.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtSenhaCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSenhaCadastro.Location = new System.Drawing.Point(40, 267);
-            this.txtSenhaCadastro.Multiline = true;
-            this.txtSenhaCadastro.Name = "txtSenhaCadastro";
-            this.txtSenhaCadastro.Size = new System.Drawing.Size(339, 37);
-            this.txtSenhaCadastro.TabIndex = 23;
+            this.txtSenha.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtSenha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(34, 317);
+            this.txtSenha.Multiline = true;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(339, 37);
+            this.txtSenha.TabIndex = 23;
             // 
             // label1
             // 
@@ -111,16 +113,16 @@
             this.label1.TabIndex = 22;
             this.label1.Text = "Nome:";
             // 
-            // txtNomeCadastro
+            // txtNome
             // 
-            this.txtNomeCadastro.BackColor = System.Drawing.Color.Gainsboro;
-            this.txtNomeCadastro.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtNomeCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNomeCadastro.Location = new System.Drawing.Point(40, 190);
-            this.txtNomeCadastro.Multiline = true;
-            this.txtNomeCadastro.Name = "txtNomeCadastro";
-            this.txtNomeCadastro.Size = new System.Drawing.Size(339, 37);
-            this.txtNomeCadastro.TabIndex = 21;
+            this.txtNome.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(40, 190);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(339, 37);
+            this.txtNome.TabIndex = 21;
             // 
             // pictureBox2
             // 
@@ -135,7 +137,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::SistemaChamado.Properties.Resources.Cliente;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(824, 516);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
@@ -155,20 +157,44 @@
             this.lbDiretorio.Text = "x";
             this.lbDiretorio.Click += new System.EventHandler(this.lbDiretorio_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Adobe Heiti Std R", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(39, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(40, 254);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(339, 37);
+            this.txtEmail.TabIndex = 31;
+            // 
             // frmTelaCadastroUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(819, 515);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.lbDiretorio);
             this.Controls.Add(this.lbFuncionario);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.lbSenha);
-            this.Controls.Add(this.txtSenhaCadastro);
+            this.Controls.Add(this.txtSenha);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtNomeCadastro);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmTelaCadastroUsuario";
@@ -188,10 +214,12 @@
         private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label lbSenha;
-        private System.Windows.Forms.TextBox txtSenhaCadastro;
+        private System.Windows.Forms.TextBox txtSenha;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtNomeCadastro;
+        private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lbDiretorio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
--- SistemaChamado/frmTelaCadastroUsuario.cs
+++ SistemaChamado/frmTelaCadastroUsuario.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace SistemaChamado
 {
     public partial class frmTelaCadastroUsuario : Form
     {
+
+        string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=dbChamado;Integrated Security=True";
         public frmTelaCadastroUsuario()
         {
             InitializeComponent();
@@ -31,7 +34,30 @@ namespace SistemaChamado
 
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
+            // Consulta SQL para inserir um usuário na tabela tblUsuario
+            string query = "INSERT INTO tblUsuario (Nome, Email, Senha) " +
+                           "VALUES (@Nome, @Email, @Senha)";
 
+            // Criando a conexão e o comando
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                // Adicionando os parâmetros com valores dos campos do formulário
+                cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text); // Você deve ter um TextBox para o email
+                cmd.Parameters.AddWithValue("@Senha", txtSenha.Text); // Você deve ter um TextBox para a senha
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Usuário cadastrado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao cadastrar usuário: " + ex.Message);
+                }
+            }
         }
     }
 }
