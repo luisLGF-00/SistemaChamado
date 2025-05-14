8d50cd885d5a19011140e73233c5c03de47d5112
Senai <Senai@CEPEAS21104035>
2025-05-13 21:00:22 -0300

    teste

--- SistemaChamado/Form1.cs
+++ SistemaChamado/Form1.cs
@@ -2,7 +2,16 @@
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Windows.Forms;
-
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 namespace SistemaChamado
 {
     public partial class frmPrincipal : Form
